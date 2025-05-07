ed614150c07b354615b09357d853ccc66270052e
Dick Chan <dickchan@Dicks-MacBook-Air.local>
2025-05-07 08:57:26 +0800

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  title: 'AI Stock agents by Knode',
+  description: 'AI Stock agents by Knode',
 };
 
 export const viewport = {
